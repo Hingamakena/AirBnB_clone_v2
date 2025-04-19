d1d5c119537504d323893f5653ed2a1a3993de05
Hingamakena <hingamakena@gmail.com>
2025-04-19 12:11:40 -0700

    instructions on how to create the bash file

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,8 @@
+to:
+"""
+Install Nginx if not installed
+create the folder /data/ if it doesn't already exist
+Create the folder /data/web_static/ if it doesn't exist
+create the folder /data/web_static/releases/ if it doesn't exist
+Create the folder /data/web_static/shared/ if it doesn't exist
+create the folder /data_web_static/releases/test/ if it doesn't exist
