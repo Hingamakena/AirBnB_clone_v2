e78a8fd08344eaa6556d9b5e2e48b7c7be0523a8
hingamakena <hingamakena@gmail.com>
2025-07-11 02:23:04 -0700

    documented the module

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,9 +6,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
-""" route to the  root folder """
+""" route to the  root folder"""
+
 def main():
+    """ return a string to the front_end of the app """
     return "Hello HBNB"
 
 if __name__ == "__main__":
+    """ run on port 5000 with host listening at 00.0.0 """
     app.run(port=5000, host="0.0.0.0")
