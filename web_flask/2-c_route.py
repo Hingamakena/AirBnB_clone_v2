234433ff56765cf4672b78d10f0e88be837ce13c
hingamakena <hingamakena@gmail.com>
2025-07-11 02:42:22 -0700

    route flask app

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-""" from flask import Flask for namespace
-    
-    initialize app with the flask Namespace """
+""" from flask import Flask for namespace """
+from flask import Flask
+
 app = Flask(__name__)
 
 
